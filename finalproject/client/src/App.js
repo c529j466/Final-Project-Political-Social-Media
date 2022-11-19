4fced180a079b967720eae58ad1bb7f3611aca08
Nllano777 <nllbr99@gmail.com>
2022-11-19 15:18:01 -0600

    added Bootstrap/Reactstrap and functional dependencies

--- finalproject/client/src/App.js
+++ finalproject/client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [data, setData] = React.useState(null);
--- finalproject/client/src/index.js
+++ finalproject/client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
