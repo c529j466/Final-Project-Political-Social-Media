42d6e61581bde8009f1f16919b1bc0803fa72bd2
Nllano777 <nllbr99@gmail.com>
2022-11-28 11:35:14 -0600

    classname

--- client/src/pages/SearchPage/searchPage.js
+++ client/src/pages/SearchPage/searchPage.js
@@ -2,26 +2,33 @@
 import React from "react";
 import function searchPage() {
     return (
-        <div class="container">
-            <div class="row mb-5">
-                <div class="col-lg-8 mx-auto">
+        <div className="container">
+
+            <div className="row mb-5">
+
+                <div className="col-lg-8 mx-auto">
+
                     <form action="">
-                        <div class="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
-                            <div class="input-group">
-                                <input type="search" placeholder="search here" class="form-control border-0 bg-light">
-                                    <div class="input-group-append">
-                                        <button id="button-addon1" type="submit" class="btn btn-link text-primary"> <i
-                                            class="fa fa-search"> </i> </button>
-                                    </div>
+                        <div className="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
+
+                            <div className="input-group">
+
+                                <input type="search" placeholder="search here" className="form-control border-0 bg-light"> </input>
+
+                                <div className="input-group-append">
+
+                                    <button id="button-addon1" type="submit" className="btn btn-link text-primary"> <i
+
+                                        className="fa fa-search"> </i> </button>
+
+                                </div>
                             </div>
                         </div>
 
                     </form>
                 </div>
-            </div>
-        </div>
+            </div >
         </div >
-
     )
 };
 export default searchPage;
