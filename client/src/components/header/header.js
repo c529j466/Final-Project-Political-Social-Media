0973e1677eeb45603204b97da10002ff29ccf56a
Your Name <Your Email>
2022-11-27 22:04:39 -0600

    added signin/signup as well as smaller components

---
+++ client/src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PCimg from './images'
+
+function Header() {
+  return (
+    <div>
+        <header>
+            <img src= {{PCimg}}></img>
+            <h3>Political Connect</h3>
+        </header>
+    </div>
+  );
+};
+
+export default Header;
