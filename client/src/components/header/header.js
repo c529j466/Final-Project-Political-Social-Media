57e321a66fc5c2f5dc6098fc16da369c5d555be4
Christian Jones <c529j466@yahoo.com>
2022-11-28 01:10:35 -0600

    updated appjs with navbar and header

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import PCimg from './images'
+import PCimg from './images/PCimg.jpg'
 
 function Header() {
   return (
     <div>
         <header>
-            <img src= {{PCimg}}></img>
+            <img src= {PCimg} alt="logo"></img>
             <h3>Political Connect</h3>
         </header>
     </div>
