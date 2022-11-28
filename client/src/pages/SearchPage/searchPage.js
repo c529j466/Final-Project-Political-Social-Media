3087cac79017aef4c6bc2bf5aabb4744220f9036
Nllano777 <nllbr99@gmail.com>
2022-11-28 16:11:42 -0600

    Pages up

---
+++ client/src/pages/SearchPage/searchPage.js
@@ -0,0 +1,35 @@
+
+import React from "react";
+import './search.css'
+import function searchPage() {
+    return (
+        <div className="container">
+
+            <div className="row mb-5">
+
+                <div className="col-lg-8 mx-auto">
+
+                    <form action="">
+                        <div className="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
+
+                            <div className="input-group">
+
+                                <input type="search" placeholder="search here" className="form-control border-0 bg-light"> </input>
+
+                                <div className="input-group-append">
+
+                                    <button id="button-addon1" type="submit" className="btn btn-link text-primary"> <i
+
+                                        className="fa fa-search"> </i> </button>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div >
+        </div >
+    )
+};
+export default searchPage;
--- client/src/pages/politicianProfile/politican.js
+++ client/src/pages/politicianProfile/politican.js
@@ -1,38 +1,69 @@
 import React from "react"
-import
-    function poliProfile() {
+import './poli.css'
 
-        return (
-            <div className="student-profile py-4">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4">
-                            <div className="card shadow-sm">
-                                <div className="card-header bg-transparent text-center">
-                                    <img className="profile-img" src="" alt="">
-                                        <h3>USERNAME</h3>
-                                </div>
-                                <div className="card-body">
-                                    <p className="mb-0"><strong className="">Following </strong>90</p>
-                                </div>
+import function poliProfile() {
+
+    return (
+        <div className="container">
+            <div className="row gx-12">
+                <div className="col-md-12 mb-12">
+                    <div className="bg-image hover-overlay">
+                        <img src="https://mdbcdn.b-cdn.net/img/new/slides/080.webp" className="img-fluid" />
+                    </div>
+                    <div className="col-12 ">
+                        <div className="card-header">;
+                            <h1 className="candidate">Politician Name
+                                <span className="material-symbols-outlined">person</span>
+                            </h1>
+                        </div>
+                        <div className="user-body">
+                            <p className="edit">Followers: 127 </p>
+                            <p className="edit">Likes: <span className="material-symbols-outlined">
+                                favorite
+                            </span>12</p>
+                            <div>
+                                <h3 className="">About me</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nisi
+                                    voluptatum vitae, veniam atque, inventore beatae repellendus vero illum
+                                    deserunt
+                                    maiores blanditiis esse saepe dolores eligendi quo expedita tempora!
+                                    Laboriosam!</p>
                             </div>
                         </div>
-                        <div className="col-lg-8">
+                        <div className=" col-md-12 mb-12">
                             <div className="card shadow-md">
-
                                 <div style="height: 26px"></div>
                                 <div className="card shadow-md">
                                     <div className="card-header bg-transparent border-0">
-                                        <h3 className="mb-0"><i className="far fa-clone pr-1"></i>Other Information</h3>
+                                        <h3 className="mb-0">Feed</h3>
                                     </div>
                                     <div className="card-body pt-0">
-                                        <button type="button" className="btn btn-outline-dark flex-grow-2">Chat</button>
-                                        <button type="button" className="btn btn-outline-dark flex-grow-2">Follow</button>
-
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                        <div>
+                                            <button type="button" className="btn btn-outline-dark flex-grow-2">
+                                                Post</button>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                            eiusmod tempor
                                             incididunt
-                                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                            nostrud exercitation
                                             ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Reprehenderit
+                                            nisi
+                                            voluptatum vitae, veniam atque, inventore beatae repellendus vero
+                                            illum
+                                            deserunt
+                                            maiores blanditiis esse saepe dolores eligendi quo expedita tempora!
+                                            Laboriosam!</p>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Reprehenderit
+                                            nisi
+                                            voluptatum vitae, veniam atque, inventore beatae repellendus vero
+                                            illum
+                                            deserunt
+                                            maiores blanditiis esse saepe dolores eligendi quo expedita tempora!
+                                            Laboriosam!</p>
                                     </div>
                                 </div>
                             </div>
@@ -40,7 +71,8 @@ import
                     </div>
                 </div>
             </div>
-        )
-    };
+        </div>
+    )
+};
 
 export default poliProfile;
